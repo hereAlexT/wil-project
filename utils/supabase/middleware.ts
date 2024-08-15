1afacb29a360802657406fb83499f33a53b297d1
Alex T <79433869+hereAlexT@users.noreply.github.com>
2024-08-15 23:23:56 +1000

    feat: allow access to homepage when user is not authenticated

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -39,6 +39,7 @@ export async function updateSession(request: NextRequest) {
 
   if (
     !user &&
+    request.nextUrl.pathname !== "/" && // Allow access to the root path
     !request.nextUrl.pathname.startsWith("/login") &&
     !request.nextUrl.pathname.startsWith("/auth")
   ) {
