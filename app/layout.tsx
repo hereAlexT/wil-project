d8726eab19c514702175d9a5d341b99733f8baff
Alex T <79433869+hereAlexT@users.noreply.github.com>
2024-08-15 23:43:29 +1000

    refactor: remove main from layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,9 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Theme>
-          <main>
-            {children}
-          </main>
+          {children}
         </Theme>
       </body>
 
