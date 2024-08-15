d54d14a6e7e458507f1263a4fc1e766e4dc1f03b
Alex T <79433869+hereAlexT@users.noreply.github.com>
2024-08-15 23:32:59 +1000

    feat: optimze login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,14 +2,13 @@
 
 import { Auth } from '@supabase/auth-ui-react';
 import { ThemeSupa } from '@supabase/auth-ui-shared';
-
 import { createClient } from '@/utils/supabase/client';
 
 const Login = () => {
     const supabase = createClient();
     return (
-        <div className="flex items-center justify-center min-h-screen">
-            <div className="w-full max-w-md">
+        <div className="flex items-center justify-center min-h-screen bg-gray-100">
+            <div className="w-full max-w-md p-8 space-y-3 bg-white rounded-xl shadow-md">
                 <Auth
                     supabaseClient={supabase}
                     appearance={{ theme: ThemeSupa }}
@@ -19,4 +18,5 @@ const Login = () => {
         </div>
     );
 };
+
 export default Login;
