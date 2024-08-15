af2bef2f541975f36231b82ea42aafe51ffe35d0
Alex T <79433869+hereAlexT@users.noreply.github.com>
2024-08-16 00:09:47 +1000

    feat: redirect to posts when user is logged

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,9 +3,24 @@
 import { Auth } from '@supabase/auth-ui-react';
 import { ThemeSupa } from '@supabase/auth-ui-shared';
 import { createClient } from '@/utils/supabase/client';
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 const Login = () => {
     const supabase = createClient();
+    const router = useRouter();
+
+    useEffect(() => {
+        const {
+            data: { subscription },
+        } = supabase.auth.onAuthStateChange((event) => {
+            if (event === "SIGNED_IN") {
+                router.replace('/posts')
+            }
+        });
+
+        return () => subscription.unsubscribe();
+    });
     return (
         <div className="flex items-center justify-center min-h-screen bg-gray-100">
             <div className="w-full max-w-md p-8 space-y-3 bg-white rounded-xl shadow-md">
