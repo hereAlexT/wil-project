cadde45a7cd445b08e2079f7ce3bf3f09d72a852
Alex T <79433869+hereAlexT@users.noreply.github.com>
2024-09-14 10:35:18 +1000

    remove about our team our story from navbar

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -18,24 +18,6 @@ const NavBar = async () => {
                         <NavigationMenu.Link className="NavigationMenuLink" href="/">
                             Home
                         </NavigationMenu.Link>
-                        <NavigationMenu.Item>
-                            <NavigationMenu.Trigger className="hover:text-gray-300 cursor-pointer">
-                                About
-                            </NavigationMenu.Trigger>
-                            <NavigationMenu.Content className="absolute top-full mt-2 bg-gray-700 p-2 rounded-lg shadow-md">
-                                <a href="#" className="block px-4 py-2 text-white hover:bg-gray-600 rounded">
-                                    Our Team
-                                </a>
-                                <a href="#" className="block px-4 py-2 text-white hover:bg-gray-600 rounded">
-                                    Our Story
-                                </a>
-                            </NavigationMenu.Content>
-                        </NavigationMenu.Item>
-                        <NavigationMenu.Item>
-                            <NavigationMenu.Trigger className="hover:text-gray-300 cursor-pointer">
-                                Contact
-                            </NavigationMenu.Trigger>
-                        </NavigationMenu.Item>
                     </NavigationMenu.List>
 
                     {/* Optional: Navigation Menu Indicator */}
